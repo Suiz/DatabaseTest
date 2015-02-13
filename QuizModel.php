d527ba7e06d332084a34c8850ecafd5217166672
chpri_000 <chpri_000@Priya>
2015-02-13 16:27:50 +0100

    Fetch the questionId and the related questiontext  based on the quizId

--- QuizModel.php
+++ QuizModel.php
@@ -1,24 +1,26 @@
 <?php
+
 require_once 'Db.php';
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 class QuizModel {
-    
-    public static function get($quizzId){
+
+    public static function get($quizzId) {
         $db = Db::getConnection();
-        $sql = "select user_id, title 
+        $sql = "select  quiz_id, user_id, title  
                  from quiz
-                 where user_id = :quiz_id";
-        
+                 where quiz_id = :quiz_id";
+
         $stmt = $db->prepare($sql);
         $stmt->bindValue(":quiz_id", $quizzId);
         $ok = $stmt->execute();
-    if ($ok) {
-      return $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($ok) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
     }
-}
-}
 
+}
